5820fd29700d110ee757e5fc6ab5f29253a0f9c5
Tyuiu.ChazovaSR@outlook.com <chazovas0@gmail.com>
2023-09-11 12:32:53 +0500

    Task1

---
+++ Tyuiu.ChazovaSR.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ChazovaSR.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Софья";
+            int age = 100;
+            Console.WriteLine("Здраствуйте меня зовут " + name+ " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
