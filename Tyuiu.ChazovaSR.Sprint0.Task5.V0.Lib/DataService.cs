b75d9143eb8c3329b3c873418c7c83bb4c1764d4
Tyuiu.ChazovaSR@outlook.com <chazovas0@gmail.com>
2023-09-11 20:27:51 +0500

    Task5

---
+++ Tyuiu.ChazovaSR.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ChazovaSR.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static double Division (int a , int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("переменная равна нулю, на ноль делить нельзя", b);
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+
+        public static double Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+}
---
+++ Tyuiu.ChazovaSR.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ChazovaSR.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ChazovaSR.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2408d079-ec25-4451-983b-9730c3a32ea2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
