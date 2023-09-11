275062a2912dcfcab8b323f2471c8596ae747298
Tyuiu.ChazovaSR@outlook.com <chazovas0@gmail.com>
2023-09-11 13:02:49 +0500

    Task2

--- Tyuiu.ChazovaSR.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.ChazovaSR.Sprint0.Task1.V0/Program.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.ChazovaSR.Sprint0.Task1.V0
             Console.WriteLine("Hello, World");
             string name = "Софья";
             int age = 100;
-            Console.WriteLine("Здраствуйте меня зовут " + name+ " мне " + age + " лет!");
+            Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " лет!");
             Console.ReadKey();
         }
     }
---
+++ Tyuiu.ChazovaSR.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ChazovaSR.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ChazovaSR.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0e88d080-218e-4087-994a-360d355fd2c9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
